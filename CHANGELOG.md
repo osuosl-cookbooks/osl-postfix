5c41386a8a6aa9e5528303957d6ac5f77e6a4259
Manatee <github@osuosl.org>
2019-03-06 20:15:58 +0000

    Automatic patch-level version bump to v1.0.1 by Jenkins

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ osl-postfix CHANGELOG
 This file is used to list changes made in each version of the
 osl-postfix cookbook.
 
+1.0.1 (2019-03-06)
+------------------
+- Add node['osl-postfix']['main'] which sets node['postfix']['main']
+
 1.0.0 (2018-07-30)
 ------------------
 - Add basic postfix client (default) & server recipes
--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ issues_url       'https://github.com/osuosl-cookbooks/osl-postfix/issues'
 source_url       'https://github.com/osuosl-cookbooks/osl-postfix'
 description      'Installs/Configures osl-postfix'
 long_description 'Installs/Configures osl-postfix'
-version          '1.0.0'
+version          '1.0.1'
 
 depends          'apt'
 depends          'postfix', '~> 5.3.0'

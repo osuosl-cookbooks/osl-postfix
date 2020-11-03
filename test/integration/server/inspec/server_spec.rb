bdc66429b5ff4c7e1c6c6c5db16576ce8fe1e55a
Robert Detjens <detjensrobert@osuosl.org>
2020-11-02 19:01:18 -0800

    Add pfcat, pfdel scripts from cfengine
    Signed-off-by: Robert Detjens <detjensrobert@osuosl.org>

--- test/integration/server/inspec/server_spec.rb
+++ test/integration/server/inspec/server_spec.rb
@@ -5,3 +5,27 @@ end
 describe iptables do
   it { should have_rule('-A smtp -p tcp -m tcp --dport 25 -j ACCEPT') }
 end
+
+describe package 'postfix-perl-scripts' do
+  it { should be_installed }
+end if os.family == 'redhat'
+
+describe file '/usr/local/bin/pfcat' do
+  it { should exist }
+  it { should be_executable }
+  its('content') { should match /# pfcat - Helper script to print out mail from the Postfix queues./ }
+end
+
+describe command '/usr/local/bin/pfcat' do
+  its('stdout') { should match /Usage: pfcat <queue id>/ }
+end
+
+describe file '/usr/local/bin/pfdel' do
+  it { should exist }
+  it { should be_executable }
+  its('content') { should match /# pfdel - Deletes message containing specified address from the Postfix queue./ }
+end
+
+describe command '/usr/local/bin/pfdel' do
+  its('stderr') { should match /Usage: pfdel <email_address>/ }
+end

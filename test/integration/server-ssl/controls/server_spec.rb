04ee76896436229677d59f4c7e81d03c8b982a3b
Lance Albertson <lance@osuosl.org>
2021-06-09 12:23:13 -0700

    Don't enable STARTTLS by default for servers
    We shouldn't enable STARTTLS on port 25 if we don't have proper certificates
    setup. This breaks inbound email since mail clients assume it's working.
    
    Specifically, it does the following:
    
    - Sets smtpd_tls_security_level to 'none'
    - Sets smtpd_use_tls to 'no'
    
    This was the previous setup we had before #25 was merged and fixes our mail
    clients/servers.
    
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- test/integration/server-ssl/controls/server_spec.rb
+++ test/integration/server-ssl/controls/server_spec.rb
@@ -2,4 +2,9 @@ control 'postfix-server-ssl' do
   describe port 465 do
     it { should be_listening }
   end
+
+  describe postfix_conf do
+    its('smtpd_tls_security_level') { should cmp 'may' }
+    its('smtpd_use_tls') { should cmp 'yes' }
+  end
 end
